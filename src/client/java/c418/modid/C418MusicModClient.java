7cfd637344d43e3b1674e952cc1a1b5e22c35663
_PioX_ <piotrmarzec127@gmail.com>
2024-09-28 20:58:06 +0200

    Add files via upload

---
+++ src/client/java/c418/modid/C418MusicModClient.java
@@ -0,0 +1,10 @@
+package c418.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class C418MusicModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/c418/modid/C418MusicMod.java
@@ -0,0 +1,24 @@
+package c418.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class C418MusicMod implements ModInitializer {
+	public static final String MOD_ID = "c-418music-mod";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
